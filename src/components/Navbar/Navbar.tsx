c2510549fa0096a48ca272afef297e7da277ce71
yousaf wazir <umbrellaman612@gmail.com>
2023-05-26 09:44:06 +0100

    added Avatar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,42 @@
-import { Braces, ChevronDown, Database, Globe, MessageCircle } from "lucide-react";
+"use client";
+
+import {
+  Braces,
+  ChevronDown,
+  Cloud,
+  Database,
+  Github,
+  Globe,
+  MessageCircle,
+  MessageSquare,
+  PlusCircle,
+  Users,
+} from "lucide-react";
 import { Button } from "../ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { LifeBuoy } from "lucide-react";
+import { LogOut } from "lucide-react";
+import { Plus } from "lucide-react";
+import { Mail } from "lucide-react";
+import { UserPlus } from "lucide-react";
+import { Keyboard } from "lucide-react";
+import { Settings } from "lucide-react";
+import { CreditCard } from "lucide-react";
+import { User } from "lucide-react";
 
 export default function Navbar() {
   return (
@@ -20,10 +50,7 @@ export default function Navbar() {
 
         <div className="items-center hidden gap-3 lg:flex">
           <Button>Contact</Button>
-          <Avatar>
-            <AvatarImage src="https://vercel.com/api/www/avatar/haa4lnxUDprtSlqYqpjTWdVL?&s=60" />
-            <AvatarFallback>UM</AvatarFallback>
-          </Avatar>
+          <AvatarProfile />
         </div>
       </nav>
     </>
@@ -86,6 +113,101 @@ const DesktopLinks = () => {
         Pricing
       </Button>
     </div>
+  );
+};
+
+const AvatarProfile = () => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar tabIndex={0}>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User className="w-4 h-4 mr-2" />
+              <span>Profile</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <CreditCard className="w-4 h-4 mr-2" />
+              <span>Billing</span>
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="w-4 h-4 mr-2" />
+              <span>Settings</span>
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Keyboard className="w-4 h-4 mr-2" />
+              <span>Keyboard shortcuts</span>
+              <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Users className="w-4 h-4 mr-2" />
+              <span>Team</span>
+            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <UserPlus className="w-4 h-4 mr-2" />
+                <span>Invite users</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>
+                    <Mail className="w-4 h-4 mr-2" />
+                    <span>Email</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <MessageSquare className="w-4 h-4 mr-2" />
+                    <span>Message</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <PlusCircle className="w-4 h-4 mr-2" />
+                    <span>More...</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuItem>
+              <Plus className="w-4 h-4 mr-2" />
+              <span>New Team</span>
+              <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Github className="w-4 h-4 mr-2" />
+            <span>GitHub</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LifeBuoy className="w-4 h-4 mr-2" />
+            <span>Support</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem disabled>
+            <Cloud className="w-4 h-4 mr-2" />
+            <span>API</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogOut className="w-4 h-4 mr-2" />
+            <span>Log out</span>
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
 
